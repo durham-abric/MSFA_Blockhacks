6a385905c433c71e961f64f5ef3e8b4c59565a7a
durham-abric <durham.abric@mail.mcgill.ca>
2018-03-24 17:56:51 -0400

    Fixing up before submission

--- Subscrypto/src/bodies/main.js
+++ Subscrypto/src/bodies/main.js
@@ -1,11 +1,10 @@
-/**
- * Created by jaewonlee on 2018. 2. 9..
- */
+
 import React, { Component } from 'react';
 import{ Panel, ButtonGroup, Button } from 'react-bootstrap';
 
 import * as data from './../data';
 import * as firebase from 'firebase';
+import * as slots from 'slots';
 
 class Main extends Component {
   constructor(props) {
@@ -13,16 +12,12 @@ class Main extends Component {
     this.state = {
       height: window.innerHeight,
       width: window.innerWidth,
-      mode: 0,
       createAccount: false,
-      newAccount: false,
+      loggingIn: false,
       login: {
         email: "",
-        password: ""
-      },
-      newUser: {
-        firstName: "",
-        lastName: "",
+        password: "",
+        ethereumAccount: ""
       }
     };
 
@@ -41,20 +36,12 @@ class Main extends Component {
       if (user) {
         if (!firebase.auth().currentUser.displayName) {
           var name = "";
-
-          if (this.state.newAccount) {
-            name = "Professor " + this.state.newUser.firstName + " " + this.state.newUser.lastName;
-          } else {
-            name = firebase.auth().currentUser.email;
-          }
-
           firebase.auth().currentUser.updateProfile({displayName: name}).then(()=>{
           }).catch(function(error) {
             //error in changing displayname
           });
         }
 
-        const adminID = "stpX94g415buRPBTRLCl9jKnbbU2"
         if (adminID === firebase.auth().currentUser.uid) {
           this.props.history.push('/admin/');
         } else if (firebase.auth().currentUser.uid) {
@@ -74,135 +61,61 @@ class Main extends Component {
     window.removeEventListener('resize', this.resize);
   }
 
-  handleButton(i){
-    if (this.state.mode === 2) {
-      firebase.database().ref('classes/').orderByChild('course/code').equalTo(this.state.login.email).once("value").then((snapshot)=> {
-        // console.log(snapshot.val());
-        if (!snapshot.val()) {
-          alert("FAILED");
-        } else {
-          snapshot.forEach(childSnapshot => {
-            for(var i = 0; i < childSnapshot.val().course.listStudent.length; i++) {
-              if (this.state.login.password === childSnapshot.val().course.listStudent[i]) {
-                data.setID(this.state.login.password);
-                data.setInfo(childSnapshot.val());
-                data.setCourseID(childSnapshot.key);
-                this.props.history.push('/student/');
-              }
-            }
-          });
-        }
-      })
-    } else {
-      this.login();
+  handleButton(){
+    if(this.state.createAccount === true){
+      this.createID();
     }
+    this.login(); 
   }
 
   main(){
     return(
-      <div className="homewrapper" style={{width:this.state.width}}>
-        <div className="homeheader" style={{height:this.state.height/3}}>
-          <h1>ProStudent</h1>
-          <h2>Direct Communication Between a Class and their Professor</h2>
-          <h3>Provides Real-time Feedback and Consensus Evaluation</h3>
-        </div>
-
-        <br/>
-        <br/>
-
-        <div className="loginwrapper">
-          <h1>Who are you?</h1>
-          <ButtonGroup>
-            <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3.3}} onClick={()=>this.setState({mode:2})}>
-              <p>Student</p>
-            </Button>
-            <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3.3}} onClick={()=>this.setState({mode:3})}>
-              <p>Professor</p>
-            </Button>
-            <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3.3}} onClick={()=>this.setState({mode:4})}>
-              <p>Admin</p>
-            </Button>
-          </ButtonGroup>
-        </div>
-
-        <br/>
-        <br/>
-
+      <div>
+        <title>Subscrypto Extension</title>
+        {/* Latest compiled and minified CSS */}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        {/* jQuery library */}
+        {/* Latest compiled JavaScript */}	
+        <style dangerouslySetInnerHTML={{__html: "\n\t\tbody{\n   \t\t\twidth:300px;\n   \t\t\theight:320px;\n\t\t\tbackground-color: #f2f2f2;\n   \t\t\t}\n\t\t  #logo{\n\t\t\t  margin-top: 20px;\n\t\t\t  margin-bottom: 12px;\n\t\t  } \n\t\t  @font-face {\n    \t\tfont-family: truenoBold;\n    \t\tsrc: url(truenosbdit.otf);\n\t\t  \t}\n\t\t  @font-face {\n    \t\tfont-family: avenirLight;\n    \t\tsrc: url(AvenirLTStd-LightOblique.otf);\n\t\t  \t}\n\t\t  @font-face {\n    \t\tfont-family: avenirBold;\n    \t\tsrc: url(AvenirLTStd-Medium.otf);\n\t\t  \t}\n\t\t  #boton{\n\t\t\t  width:130px;\n\t\t\t  height: 40px;\n\t\t\t  background-color: #c61919 !important;\n\t\t\t  border-color: #c61919 !important;\n\t\t\t  font-family: TruenoBold;\n\t\t\t  margin-top: 3px;\n\t\t\t  margin-left: 5px;\n\t\t\t  \n\t\t  }\n\t\t  #textuno{\n\t\t\t  font-family:avenirLight;\n\t\t\t  text-decoration: underline;\n\t\t\t  padding-top: 8px;\n\t\t  }\n\t\t  #forminput{\n\t\t\t  width:270px;\n\t\t\t  height: 45px;\n\t\t\t  margin-top:0px;\n\t\t\t  margin-bottom:8px;\n\t\t\t\n\t\t  }\n\t\t  #inputlabels{\n\t\t\t  font-family:avenirBold;\n\t\t\t  font-weight: bold;\n\t\t  }\n\t" }} /> 
+        <center>
+          <img src="lineal.png" width={280} id="logo" />
+        </center>	
+        <center>	
+          <table>
+            <tbody><tr>
+                <td> <p id="inputlabels"> Email </p> </td>
+              </tr>
+              <tr>
+                <td><input type="email" id="forminput" className="form-control" aria-describedby= "emailHelp" placeholder= "Enter email addrss" onChange={(e)=> this.setState({login: {...this.state.login, email: e.target.value}})}/> </td>
+              </tr>
+              <tr>
+                <td> <p id="inputlabels"> Password </p> </td>
+              </tr>
+              <tr>
+                <td>
+                  <input className="form-control" id="forminput" placeholder="Password" onChange={(e)=> this.setState({login: {...this.state.login, ethereumAccount: e.target.value}})}/>
+                </td>
+              </tr>              
+            </tbody></table>	
+          <table>
+            <tbody><tr>
+                <td> 
+                  <a href="slots.html"> 
+                    <button type="button" className="btn btn-primary btn-lg btn-block" id="loginButton" onClick={()=>{this.setState({loggingIn : true, createAccount : false}).then(this.handleButton())}}>LOGIN</button>
+                  </a>	
+                </td>
+                <td> <button type="button" className="btn btn-primary btn-lg btn-block" id="SignUpButton" onClick={()=>{this.setState({loggingIn : false, createAccount : true}).then(this.handleButton())}}>SIGN UP</button></td>
+              </tr>	
+            </tbody></table>
+          <p id="textuno"> I forgot my password</p>	
+        </center>  	
       </div>
-    )
-  }
-
-  panel(mode) {
-    //mode 1 login
-    //mode 2 create account as student
-    //mode 3 create account as professor,
-    var title = null;
-    var topInput = null;
-    var bottomInput = null;
-    switch(mode) {
-      case 2:
-        title = 'Login To Class Portal';
-        topInput = 'Access Code (Course Code)';
-        bottomInput = 'Student ID';
-        break;
-      case 3:
-        title = 'Login To Professor Account';
-        topInput = 'Email';
-        bottomInput = 'Password';
-        break;
-      case 4:
-        title = 'Login To Administrator Account';
-        topInput = 'Username';
-        bottomInput = 'Password';
-        break;
-      default:
-        // Do nothing
-        break;
-    }
-
-    return (
-      <div className="center form" style={{flexDirection:'column'}}>
-        <p className="title">{title}</p>
-        <hr className="formseperator"/>
-
-        <p className="formlabel">{topInput}</p>
-        <input className="forminput" onChange={(e)=> this.setState({login:{...this.state.login, email: e.target.value}})}/>
-
-        <br/>
-
-        <p className="formlabel">{bottomInput}</p>
-        <input type='password' className="forminput" onChange={(e)=> this.setState({login: {...this.state.login, password: e.target.value}})}/>
-
-        <br/>
-        <br/>
-
-        <ButtonGroup>
-          <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3}} onClick={()=>this.handleButton(this.state.mode)}>
-            <p>Login</p>
-          </Button>
-          <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3}} onClick={()=>this.setState({mode: 0, login: {...this.state.login, password: null}})}>
-            <p>Cancel</p>
-          </Button>
-        </ButtonGroup>
-
-        <br/>
-
-        {
-          this.state.mode === 3 ?
-            <Button bsSize="large" className="loginbutton" style={{width: this.state.width/3}} onClick={()=>this.setState({createAccount: true})}>
-              <p>Create Account</p>
-            </Button>
-          :
-            null
-        }
-      </div>
-    )
+    
+  )
   }
 
   createID(type) {
-    firebase.auth().createUserWithEmailAndPassword(this.state.login.email, this.state.login.password).then(()=>{
-      alert('Your account has been created');
-      this.setState({newAccount:true});
+    firebase.auth().createUserWithEmailAndPassword(this.state.login.email, this.state.login.password).then((this.login())=>{
     }).catch(function(error) {
       alert(error);
     });
@@ -219,81 +132,6 @@ class Main extends Component {
     ).catch((error)=>alert(error +"... Try loggin in again!"));
   }
 
-  render() {
-    if(this.state.mode === 2 || this.state.mode === 4) {
-      return (
-        <div className="App">
-          <Panel style={{position:'absolute',
-            top:this.state.height/8,
-            left:this.state.width>1000?this.state.width/3:this.state.width/8,
-            width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
-            height:this.state.width>1000?this.state.height*7/8:this.state.height*13/16}}>
-            {this.panel(this.state.mode)}
-          </Panel>
-        </div>
-      )
-    } else if (this.state.mode === 3) {
-      return (
-        <div className="App">
-          {
-            !this.state.createAccount ?
-              <Panel style={{position:'absolute',
-                top:this.state.height/8,
-                left:this.state.width>1000?this.state.width/3:this.state.width/8,
-                width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
-                height:this.state.width>1000?this.state.height*7/8:this.state.height*13/16}}>
-                {this.panel(this.state.mode)}
-              </Panel>
-            :
-              <Panel style={{position:'absolute',
-                top:this.state.height/8,
-                left:this.state.width>1000?this.state.width/3:this.state.width/8,
-                width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
-                height:this.state.width>1000?this.state.height*7/8:this.state.height*13/16}}>
-
-                <div className="center form" style={{flexDirection:'column'}}>
-                  <p className="title">Create Professor Account</p>
-                  <hr className="formseperator"/>
-
-                  <p className="formlabel">First Name</p>
-                  <input className="forminput" onChange={(e)=> this.setState({newUser: {...this.state.newUser, firstName: e.target.value}})}/>
-
-                  <br/>
-
-                  <p className="formlabel">Last Name</p>
-                  <input className="forminput" onChange={(e)=> this.setState({newUser: {...this.state.newUser, lastName: e.target.value}})}/>
-
-                  <br/>
-
-                  <p className="formlabel">Email</p>
-                  <input className="forminput" onChange={(e)=> this.setState({login: {...this.state.login, email: e.target.value}})}/>
-
-                  <br/>
-
-                  <p className="formlabel">Password</p>
-                  <input type='password' className="forminput" onChange={(e)=> this.setState({login: {...this.state.login, password: e.target.value}})}/>
-
-                  <br/>
-                  <br/>
-
-                  <ButtonGroup>
-                    <Button bsSize="large" className="loginbutton" style={{width:this.state.width/3}} onClick={()=>{this.createID()}}>
-                      <p>Create Account</p>
-                    </Button>
-                    <Button bsSize="large" className="loginbutton" style={{width:this.state.width/3}} onClick={()=>this.setState({createAccount: false})}>
-                      <p>Cancel</p>
-                    </Button>
-                  </ButtonGroup>
-
-                </div>
-              </Panel>
-            }
-          </div>
-        )
-      } else {
-        return(this.main())
-      }
-    }
 }
 
 export default Main
--- Subscrypto/src/index.js
+++ Subscrypto/src/index.js
@@ -13,10 +13,6 @@ import './index.css';
 
 //bodies
 import MainPage from './bodies/main'
-import NavigationBar from './navbar/navigationBar'
-import StudentLanding from './bodies/studentLanding'
-import ProfLanding from './bodies/professorLanding'
-import AdminLanding from './bodies/adminLanding'
 
 const Redirection = () => (
   <Redirect to={"/main"}/>
@@ -24,18 +20,10 @@ const Redirection = () => (
 
 firebaseFunctions.init();
 
-//<Route path="/app/*" component={MainPage}/>
-
 ReactDOM.render(
   <div>
-    <NavigationBar/>
     <Router>
       <Switch>
-        <Route exact path="/main" component={MainPage} />
-        <Route exact path="/student/*" component={StudentLanding} />
-        <Route exact path="/prof/*" component={ProfLanding} />
-        <Route exact path="/admin/*" component={AdminLanding} />
-        <Route path="/*" component={Redirection} />
       </Switch>
     </Router>
   </div>,
